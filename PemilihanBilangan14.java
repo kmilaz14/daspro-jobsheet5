a788c7d5c9db9c054796f1be1f77626b75f0f264
kmilaz14 <kamilazhwa@gmail.com>
2024-09-26 09:29:53 +0800

    pertanyaan 1

--- PemilihanBilangan14.java
+++ PemilihanBilangan14.java
@@ -7,12 +7,8 @@ public class PemilihanBilangan14 {
         System.out.print("Masukkan sebuah angka: ");
         int angka = sc.nextInt();
 
-        if (angka % 2 == 0) 
-        {
-            System.out.println("Angka " + angka + " termasuk bilangan genap");
-        }
-        else {
-            System.out.println("Angka " + angka + " termasuk bilangan ganjil");
-        }
+        String hasil = (angka % 2 == 0) ? "Angka " + angka + " termasuk bilangan genap" : "Angka " + angka + " termasuk bilangan ganjil";
+
+        System.out.println(hasil);
     }
 }
